e909a514ffbbf550577af1ef1613ed356e815966
ozgeKrt <59333316+ozgeKrt@users.noreply.github.com>
2020-05-17 17:55:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ob_stringDizi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string yazi = "Mer!habalar";
+            //string nesnelerinin tutulduğu karakterlere dizi erişim operatörleri ([]) ile erişilebilir.
+            Console.WriteLine("yazi[0].....:{0}", yazi[0]);   //stringin ilk rakamını çıkarır.
+            Console.WriteLine("yazi[1].....:{0}", yazi[1]);
+            Console.WriteLine("yazi[2].....:{0}", yazi[2]);
+            Console.WriteLine("Yazının uzunluğu kaçtır?.....:{0}", yazi.Length);  //yazının uzunluğu sayısal
+            
+            for(int i=0; i<yazi.Length;i++)
+            {
+                Console.WriteLine("Sıradaki harf..:{0}", yazi[i]);
+            }
+
+        }
+    }
+}
